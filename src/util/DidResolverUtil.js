800d98b408ae444e4045306a99751918f0baf3d8
nitro-neal <groessler24@gmail.com>
2020-08-04 15:32:11 -0500

    Adding cors proxy to access uniresovler

--- src/util/DidResolverUtil.js
+++ src/util/DidResolverUtil.js
@@ -3,12 +3,23 @@ import { getResolver } from "ethr-did-resolver";
 
 class DidResolverUtil {
   static async getInfoByDocumentDid(did) {
-    const UNIRESOLVER_API = "https://uniresolver.io/1.0/identifiers/";
+    const UNIRESOLVER_API =
+      "https://cors-anywhere.herokuapp.com/https://uniresolver.io/1.0/identifiers/";
     const response = await fetch(UNIRESOLVER_API + did);
     const jsonResponse = await response.json();
-    const {didDocument} = {...jsonResponse};
-    const vpJwt = (didDocument.service && didDocument.service.length > 0 && didDocument.service[0].serviceEndpoint) ? didDocument.service[0].serviceEndpoint : "";
-    const ownerPublicKey = (didDocument.publicKey && didDocument.publicKey.length > 0 && didDocument.publicKey[0].id) ? didDocument.publicKey[0].id : "";
+    const { didDocument } = { ...jsonResponse };
+    const vpJwt =
+      didDocument.service &&
+      didDocument.service.length > 0 &&
+      didDocument.service[0].serviceEndpoint
+        ? didDocument.service[0].serviceEndpoint
+        : "";
+    const ownerPublicKey =
+      didDocument.publicKey &&
+      didDocument.publicKey.length > 0 &&
+      didDocument.publicKey[0].id
+        ? didDocument.publicKey[0].id
+        : "";
     return { vpJwt, ownerPublicKey };
   }
 
